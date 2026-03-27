04c94a7c074870baebba844eec460c67d14bfa82
DerekTBernal <derek.bernal@smc.pshs.edu.ph>
2026-03-27 14:13:04 +0800

    Update Gr7Implementer.java

--- Group7_PalmDrive/src/Gr7Cytophobia/Gr7Implementer.java
+++ Group7_PalmDrive/src/Gr7Cytophobia/Gr7Implementer.java
@@ -1,15 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Gr7Cytophobia;
 
-import Cytophobia.*;
-
-/**
- *
- * @author asus
- */
+// Members: Catingub, Nierves, Bernal
 public class Gr7Implementer {
     
     public static String assetsFolder = "assets/";
